322279dfb430dbf5dcf01dea44dfc2a5addb386f
Simon Mavsar <simon.mavsar@gmail.com>
2013-10-16 19:23:55 +0200

    Working on configuration....

--- nodetest1/config/index.js
+++ nodetest1/config/index.js
@@ -5,5 +5,5 @@ var config = {
 }
 
 module.exports = function(mode) {
-    return config[mode||process.argv[2]||'local'||config.local];
+    return config[mode || process.argv[2] || 'local' || config.local];
 }
