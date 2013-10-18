543fcff90768c16ddb66f468f1d3f904d52ec63e
Simon Mavsar <simon.mavsar@gmail.com>
2013-10-18 19:43:05 +0200

    Mongodb test updated.

--- nodetest1/tests/mongodb.spec.js
+++ nodetest1/tests/mongodb.spec.js
@@ -2,9 +2,9 @@
  * MongoDB test coverage.
  */
 
+ var config = require('../config')('local');
  describe("MongoDB", function() {
    it("is there a server running", function(next) {
-     var config = require('../config')('local');
      var MongoClient = require('mongodb').MongoClient;
 	 var mongoDbConnection = 'mongodb://'+ config.mongodb.host + ':' + config.mongodb.port + '/' + config.mongodb.dbName;
 	 MongoClient.connect(mongoDbConnection, function(err, db) {
