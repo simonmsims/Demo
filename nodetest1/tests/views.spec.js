697ba513eae1ca5617b7181ed615e7506c15cf77
Simon Mavsar <simon.mavsar@gmail.com>
2013-10-18 19:58:49 +0200

    Added base view.

---
+++ nodetest1/tests/views.spec.js
@@ -0,0 +1,30 @@
+/*
+ * Views tests.
+ */
+
+var View = require("../views/base/base.js");
+describe("Base view", function() {
+  it("create and render new view", function(next) {
+    var responseMockup = {
+      render: function(template, data) {
+        expect(template).toBe('template-file');
+        expect(data.myProperty).toBe('value');
+        next();
+      }
+    }
+    var v = new View(responseMockup, 'template-file');
+    v.render({myProperty: 'value'});
+  });
+  it("should be extendable", function(next) {
+    var v = new View();
+    var OtherView = v.extend({
+      render: function(data) {
+        expect(data.prop).toBe('yes');
+        next();
+      }
+    });
+    var otherViewInstance = new OtherView();
+    expect(otherViewInstance.render).toBeDefined();
+    otherViewInstance.render({prop: 'yes'});
+  });
+});
