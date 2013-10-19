d04ba2db0046121cd901d84f5091a952495772b6
Simon Mavsar <simon.mavsar@gmail.com>
2013-10-19 16:44:29 +0200

    Workspace updated - idents.

---
+++ nodetest1/controllers/base/index.js
@@ -0,0 +1,12 @@
+/*
+ * Base controller.
+ */
+var _ = require("underscore");
+module.exports = {
+  name: "base",
+  extend: function(child) {
+      return _.extend({}, this, child);
+    },
+    run: function(req, res, next) {
+  }
+}
